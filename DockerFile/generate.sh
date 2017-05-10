46ab1ce5d673ead9c987919365f146015e6e0b19
Christof Fetzer <christof.fetzer@gmail.com>
2017-05-10 16:01:24 +0200

    .

---
+++ DockerFile/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# (C) Christof Fetzer, 2017
+
+set -x -e
+
+export TAG="again"
+export FULLTAG="sconecuratedimages/helloworld:$TAG"
+
+echo "generating image $FULLTAG"
+
+docker build -t $FULLTAG .
+
+docker push $FULLTAG
+
